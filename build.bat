1d1970830715d8751a0edf71c05c06925ed87b53
Paul Skeen <plesk@bigpond.com>
2020-03-11 00:08:03 +1100

    Build and Icon Updates
    Further changes to build scripts and minor updates to graphics.

--- build.bat
+++ build.bat
@@ -6,10 +6,7 @@ rem Compile Resources
 CALL python -m PyQt5.pyrcc_main -o resources.py resources.qrc
 
 rem Copy all files
-xcopy *.png getwkt\
 xcopy *.py getwkt\
 xcopy *.ui getwkt\
 xcopy licence.txt getwkt\
 xcopy metadata.txt getwkt\
-
-pause
