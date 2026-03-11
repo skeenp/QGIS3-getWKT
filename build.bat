51d30afe132fe7b33c5622fcb87e7f65eefd1197
skeenp <plesk@bigpond.com>
2026-03-11 19:28:16 +1100

    Rename plugin from getwkt4 to getwkt
    Reverting plugin name to QGIS3-getWKT (even though its for QGIS4) due to requirements of plugin on QGIS portal.

--- build.bat
+++ build.bat
@@ -12,12 +12,12 @@ rem Replace PyQt5 imports with PyQt6 imports for Qt6 compatibility
 powershell -Command "(Get-Content resources.py) -replace 'from PyQt5', 'from PyQt6' | Set-Content resources.py"
 
 rem Clean Build
-rmdir /s /q QGIS4-getWKT
-mkdir QGIS4-getWKT
+rmdir /s /q QGIS3-getWKT
+mkdir QGIS3-getWKT
 
 rem Copy all files
-xcopy *.py QGIS4-getWKT\
-xcopy *.ui QGIS4-getWKT\
-xcopy LICENSE QGIS4-getWKT\
-xcopy metadata.txt QGIS4-getWKT\
-xcopy wkt.png QGIS4-getWKT\
+xcopy *.py QGIS3-getWKT\
+xcopy *.ui QGIS3-getWKT\
+xcopy LICENSE QGIS3-getWKT\
+xcopy metadata.txt QGIS3-getWKT\
+xcopy icon.png QGIS3-getWKT\
--- debug.bat
+++ debug.bat
@@ -2,8 +2,8 @@ rem Build Plugin
 call build.bat
 
 rem Clean old plugin
-rmdir /s /q "%APPDATA%\QGIS\QGIS4\profiles\default\python\plugins\getwkt4_dev"
-mkdir "%APPDATA%\QGIS\QGIS4\profiles\default\python\plugins\getwkt4_dev"
+rmdir /s /q "%APPDATA%\QGIS\QGIS4\profiles\default\python\plugins\getwkt_dev"
+mkdir "%APPDATA%\QGIS\QGIS4\profiles\default\python\plugins\getwkt_dev"
 
 rem Move plugin to plugins folder
-xcopy /s /y QGIS4-getWKT "%APPDATA%\QGIS\QGIS4\profiles\default\python\plugins\getwkt4_dev\"
+xcopy /s /y QGIS3-getWKT "%APPDATA%\QGIS\QGIS4\profiles\default\python\plugins\getwkt_dev\"
